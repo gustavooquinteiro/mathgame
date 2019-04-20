d5c671e8a3def029788ddb3a5ddb9dab6d1a6593
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-04-19 22:30:05 -0300

    Atualizando setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
+readme = open('README.md').read()
 setup(name= 'mathgame',
-      version='0.1',
-      description='A 2D math game', 
+      version='1.0',
+      description='A platform math game', 
       url='http://github.com/gustavooquinteiro/mathgame',
       author='Gustavo Oliveira Quinteiro',
       author_email='gustavooquinteiro@outlook.com',
-      license='', 
+      long_description = readme,
+      license='MIT', 
       packages=['mathgame'],
       zip_safe=False)
