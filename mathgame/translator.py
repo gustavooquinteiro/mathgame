62ca0c7b9144a20962d587b5771a0bec6c6d51cb
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-05-20 07:42:01 -0300

    Melhorando o tradutor do jogo

--- mathgame/translator.py
+++ mathgame/translator.py
@@ -1,30 +1,36 @@
 import platform
 import gettext
+import os
 
-def getSystemLanguage(system):
+CONST = 'constants' 
+LOCALE = 'locale'
+
+def getSystemLanguage(system=platform.system()):
     import locale
     if system is 'Windows':
         import ctypes
         windll = ctypes.windll.kernel32
         return locale.windows_locale[windll.GetUserDefaultUILanguage()]
     else:
-        locale.setlocale(locale.LC_ALL, "")
+        locale.setlocale(locale.LC_ALL, "")        
         return locale.getlocale(locale.LC_MESSAGES)[0]
-
-
-
-language = getSystemLanguage(platform.system())
-try:
-    translation_constants = gettext.translation(
-        'constants', 
-        localedir='locale', 
-        languages=[language]
-        )
-except FileNotFoundError as error:
-    translation_constants = gettext.translation(
-        'constants', 
-        localedir='locale',
-        languages=['en']
-        )
-finally:
-    translation_constants.install()
+    
+if __name__ == "__main__":
+    print(gettext.find(CONST, 
+                    localedir=LOCALE,
+                    languages=[getSystemLanguage()], 
+                    all=True))
+else:
+    language = getSystemLanguage()
+    if gettext.find(CONST, 
+                    localedir=LOCALE, 
+                    languages=[language], 
+                    all=True):
+        gettext.translation(CONST,
+                            localedir=LOCALE, 
+                            languages=[language]).install()        
+    else:
+        gettext.translation(CONST,
+                            localedir=LOCALE,
+                            languages=['en']).install()
+        
